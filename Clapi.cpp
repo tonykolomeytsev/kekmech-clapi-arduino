2d81f2e345f7fafd830c839972b51836bb9f27f4
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2019-05-11 00:01:26 +0300

    Initial commit

---
+++ Clapi.cpp
@@ -0,0 +1,110 @@
+#include "Arduino.h"
+#include "Clapi.h"
+
+Api* Clapi::query(const char* key, const char* value) {
+    checkFirstParam();
+    Serial.print("\"");
+    Serial.print(key);
+    Serial.print("\":\"");
+    Serial.print(value);
+    Serial.print("\"");
+    return this;
+}
+
+Api* Clapi::query(const char* key, const int value) {
+    checkFirstParam();
+    Serial.print("\"");
+    Serial.print(key);
+    Serial.print("\":");
+    Serial.print(value, DEC);
+    return this;
+}
+
+Api* Clapi::query(const char* key, const long value) {
+    checkFirstParam();
+    Serial.print("\"");
+    Serial.print(key);
+    Serial.print("\":");
+    Serial.print(value, DEC);
+    return this;
+}
+
+Api* Clapi::query(const char* key, const float value) {
+    checkFirstParam();
+    Serial.print("\"");
+    Serial.print(key);
+    Serial.print("\":");
+    Serial.print(static_cast<double>(value), 16);
+    return this;
+}
+
+Api* Clapi::query(const char* key, const double value) {
+    checkFirstParam();
+    Serial.print("\"");
+    Serial.print(key);
+    Serial.print("\":");
+    Serial.print(value, 16);
+    return this;
+}
+
+Api* Clapi::query(const char* key, const char value) {
+    checkFirstParam();
+    Serial.print("\"");
+    Serial.print(key);
+    Serial.print("\":\"");
+    Serial.print(value);
+    Serial.print("\"");
+    return this;
+}
+
+Api* Clapi::query(const char* key, const bool value) {
+    checkFirstParam();
+    Serial.print("\"");
+    Serial.print(key);
+    Serial.print("\":");
+    Serial.print(value ? "true" : "false");
+    return this;
+}
+
+void Clapi::send() {
+    Serial.println("}");
+    firstParam = true;
+}
+
+void Clapi::processInput(void (*listener)(int code, int argsCount, float args[])) {
+	if (!Serial.available()) return;
+    // если не проинициализировались, то ничего не делаем
+    if (!initialized) return;
+    // когда для чтения становится доступно 2 байта, тогда продолжаем
+    while (Serial.available() < 2); // waiting
+    int code = Serial.read();
+    int argsCount = Serial.read();
+
+    if (argsCount == 0) {
+        listener(code, argsCount, nullptr);
+    } else {
+         // количество байт, которое займут аргументы функции
+        int bytesToWait = argsCount * 4;
+        // когда дождёмся всех аргументов, тогда продолжим
+        while (Serial.available() < bytesToWait); // waiting
+
+        // выделяем память под массив агрументов
+        float *args = new float[argsCount];
+        for (int i = 0; i < argsCount; i++) {
+            union { char bin[4]; float d; } doubleAsBytes;
+
+            // просто последовательно пихаем 8 байт в double ячейку
+            for (int j = 0; j < 4; j++) {
+                doubleAsBytes.bin[j] = int(Serial.read());
+            }
+            // засовываем расшифрованный double в аргументы
+            args[i] = doubleAsBytes.d;
+        }
+        listener(code, argsCount, args);
+        delete[] args;
+    }
+}
+
+void Clapi::setMessageListener(void (*listener)(int code, int argsCount, float args[])) {
+    this->listener = listener;
+}
---
+++ Clapi.h
@@ -0,0 +1,43 @@
+
+#include "Arduino.h"
+
+
+class Api {
+private:
+    void (*listener)(int code, int argsCount, float args[]);
+
+protected:
+    bool firstParam = true;
+    bool initialized = false;
+    
+    void checkFirstParam() {
+        if (firstParam) {
+            Serial.print("{");
+        } else {
+            Serial.print(",");
+        }
+        firstParam = false;
+    }
+
+public:
+    
+    void init() {
+        Serial.begin(115200);
+        Serial.setTimeout(16);
+        delay(300);
+        initialized = true;
+    }
+
+    Api* param(const char* key, const char* value);
+    Api* param(const char* key, const int value);
+    Api* param(const char* key, const long value);
+    Api* param(const char* key, const float value);
+    Api* param(const char* key, const double value);
+    Api* param(const char* key, const char value);
+    Api* param(const char* key, const bool value);
+    
+    void send();
+
+    void processInput();
+    void setMessageListener(void (*listener)(int code, int argsCount, float args[]));
+};
